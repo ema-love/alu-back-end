a1fa86d13c9a97513a3bbab2a3d46b01647c9336
Erioluwa <e.akintayo@alustudent.com>
2025-11-29 21:47:20 +0200

    #

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,24 +8,24 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit(1)
-    
+
     employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
-    
+
     # Get employee information
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
     username = user_data.get("username")
-    
+
     # Get employee's TODO list
     todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
     todos_data = todos_response.json()
-    
+
     # Write to CSV file
-    filename = f"{employee_id}.csv"
+    filename = "{}.csv".format(employee_id)
     with open(filename, mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        
+
         for task in todos_data:
             writer.writerow([
                 employee_id,
