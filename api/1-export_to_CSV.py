a753392aded93fa846662e2b97bfc1a28c3cb15b
Erioluwa <e.akintayo@alustudent.com>
2025-11-29 21:36:16 +0200

    #

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,52 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that exports employee TODO list data to CSV format
-"""
+"""Script to export employee TODO list to CSV format"""
 import csv
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         sys.exit(1)
     
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        sys.exit(1)
-    
-    # API base URL
+    employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
     
-    # Fetch employee data
-    user_url = f"{base_url}/users/{employee_id}"
-    user_response = requests.get(user_url)
-    
-    if user_response.status_code != 200:
-        sys.exit(1)
-    
+    # Get employee information
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
     username = user_data.get("username")
     
-    # Fetch TODO list for the employee
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
+    # Get employee's TODO list
+    todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
+    todos_data = todos_response.json()
     
-    if todos_response.status_code != 200:
-        sys.exit(1)
-    
-    todos = todos_response.json()
-    
-    # Export to CSV
+    # Write to CSV file
     filename = f"{employee_id}.csv"
-    
-    with open(filename, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    with open(filename, mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         
-        for task in todos:
+        for task in todos_data:
             writer.writerow([
-                str(employee_id),
+                employee_id,
                 username,
                 str(task.get("completed")),
                 task.get("title")
